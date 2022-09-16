f0cb688a9b3d0cc9f47c029828478242b8430a5f
krish-98 <muralikrishnanm98@gmail.com>
2022-09-16 13:49:37 +0530

    search bar added

--- src/Components/home/Search.jsx
+++ src/Components/home/Search.jsx
@@ -1,7 +1,17 @@
 import React from "react"
 
 const Search = () => {
-  return <div>Search</div>
+  return (
+    <div className="border-b">
+      <div>
+        <input
+          className="w-full outline-none border-none px-2 bg-purple-700 text-white py-2  placeholder:text-gray-200"
+          type="text"
+          placeholder="Find a user"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Search
