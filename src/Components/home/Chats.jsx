63cd810531957433c930a700d0d46597037e6500
krish-98 <muralikrishnanm98@gmail.com>
2022-09-16 13:50:22 +0530

    User chats design completed

--- src/Components/home/Chats.jsx
+++ src/Components/home/Chats.jsx
@@ -1,7 +1,82 @@
 import React from "react"
+import User2 from "../../Assets/user2.jpg"
 
 const Chats = () => {
-  return <div>Chats</div>
+  return (
+    <div className="bg-purple-700 h-full">
+      <div className="flex items-center gap-2 py-3 px-1 hover:bg-purple-400 cursor-pointer">
+        <img
+          className="w-10 h-10 object-cover rounded-full cursor-pointer"
+          src={User2}
+          alt="user profile icon"
+        />
+        <div className="text-white">
+          <h3 className="font-bold">Tyler</h3>
+          <p className="text-sm">Okay, bye</p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2 py-3 px-1 hover:bg-purple-400 cursor-pointer">
+        <img
+          className="w-10 h-10 object-cover rounded-full cursor-pointer"
+          src={User2}
+          alt="user profile icon"
+        />
+        <div className="text-white">
+          <h3 className="font-bold">Tyler</h3>
+          <p className="text-sm">Okay, bye</p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2 py-3 px-1 hover:bg-purple-400 cursor-pointer">
+        <img
+          className="w-10 h-10 object-cover rounded-full cursor-pointer"
+          src={User2}
+          alt="user profile icon"
+        />
+        <div className="text-white">
+          <h3 className="font-bold">Tyler</h3>
+          <p className="text-sm">Okay, bye</p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2 py-3 px-1 hover:bg-purple-400 cursor-pointer">
+        <img
+          className="w-10 h-10 object-cover rounded-full cursor-pointer"
+          src={User2}
+          alt="user profile icon"
+        />
+        <div className="text-white">
+          <h3 className="font-bold">Tyler</h3>
+          <p className="text-sm">Okay, bye</p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2 py-3 px-1 hover:bg-purple-400 cursor-pointer">
+        <img
+          className="w-10 h-10 object-cover rounded-full cursor-pointer"
+          src={User2}
+          alt="user profile icon"
+        />
+        <div className="text-white">
+          <h3 className="font-bold">Tyler</h3>
+          <p className="text-sm">Okay, bye</p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2 py-3 px-1 hover:bg-purple-400 cursor-pointer">
+        <img
+          className="w-10 h-10 object-cover rounded-full cursor-pointer"
+          src={User2}
+          alt="user profile icon"
+        />
+        <div className="text-white">
+          <h3 className="font-bold">Tyler</h3>
+          <p className="text-sm">Okay, bye</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Chats
--- src/Components/home/Sidebar.jsx
+++ src/Components/home/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import Navbar from "../home/Navbar"
+import Search from "../home/Search"
+import Chats from "../home/Chats"
 
 const Sidebar = () => {
   return (
     <div className="flex-1 bg-purple-300">
       <Navbar />
+      <Search />
+      <Chats />
     </div>
   )
 }
