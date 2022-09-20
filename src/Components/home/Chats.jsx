1b14e03ddd368fb28aeba547accb0fdc56377ab7
krish-98 <muralikrishnanm98@gmail.com>
2022-09-20 14:34:18 +0530

    fetch chat conversations

--- src/Components/home/Chats.jsx
+++ src/Components/home/Chats.jsx
@@ -1,80 +1,47 @@
-import React from "react"
-import User2 from "../../Assets/user2.jpg"
+import React, { useContext, useEffect, useState } from "react"
+import { db } from "../../firebase.config"
+import { doc, onSnapshot } from "firebase/firestore"
+import { AuthContext } from "../../context/AuthContext"
 
 const Chats = () => {
+  const [chats, setChats] = useState([])
+  const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data())
+        console.log(doc.data())
+      })
+
+      return () => {
+        unsub()
+      }
+    }
+
+    currentUser.uid && getChats()
+  }, [currentUser.uid])
+
   return (
     <div className="bg-purple-700 h-full">
-      <div className="flex items-center gap-2 py-3 px-3 hover:bg-purple-400 cursor-pointer">
-        <img
-          className="w-10 h-10 object-cover rounded-full cursor-pointer"
-          src={User2}
-          alt="user profile icon"
-        />
-        <div className="text-white">
-          <h3 className="font-bold">Tyler</h3>
-          <p className="text-sm">Okay, bye</p>
+      {Object.entries(chats)?.map((chat) => (
+        <div
+          key={chat[0]}
+          className="flex items-center gap-2 py-3 px-3 hover:bg-purple-400 cursor-pointer"
+        >
+          <img
+            className="w-10 h-10 object-cover rounded-full cursor-pointer"
+            src={chat[1].userInfo?.photoURL}
+            alt="user profile icon"
+          />
+          <div className="text-white">
+            <h3 className="font-bold capitalize">
+              {chat[1].userInfo?.displayName}
+            </h3>
+            <p className="text-sm">{chat[1].lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
-
-      <div className="flex items-center gap-2 py-3 px-3 hover:bg-purple-400 cursor-pointer">
-        <img
-          className="w-10 h-10 object-cover rounded-full cursor-pointer"
-          src={User2}
-          alt="user profile icon"
-        />
-        <div className="text-white">
-          <h3 className="font-bold">Tyler</h3>
-          <p className="text-sm">Okay, bye</p>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-2 py-3 px-3 hover:bg-purple-400 cursor-pointer">
-        <img
-          className="w-10 h-10 object-cover rounded-full cursor-pointer"
-          src={User2}
-          alt="user profile icon"
-        />
-        <div className="text-white">
-          <h3 className="font-bold">Tyler</h3>
-          <p className="text-sm">Okay, bye</p>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-2 py-3 px-3 hover:bg-purple-400 cursor-pointer">
-        <img
-          className="w-10 h-10 object-cover rounded-full cursor-pointer"
-          src={User2}
-          alt="user profile icon"
-        />
-        <div className="text-white">
-          <h3 className="font-bold">Tyler</h3>
-          <p className="text-sm">Okay, bye</p>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-2 py-3 px-3 hover:bg-purple-400 cursor-pointer">
-        <img
-          className="w-10 h-10 object-cover rounded-full cursor-pointer"
-          src={User2}
-          alt="user profile icon"
-        />
-        <div className="text-white">
-          <h3 className="font-bold">Tyler</h3>
-          <p className="text-sm">Okay, bye</p>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-2 py-3 px-3 hover:bg-purple-400 cursor-pointer">
-        <img
-          className="w-10 h-10 object-cover rounded-full cursor-pointer"
-          src={User2}
-          alt="user profile icon"
-        />
-        <div className="text-white">
-          <h3 className="font-bold">Tyler</h3>
-          <p className="text-sm">Okay, bye</p>
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
