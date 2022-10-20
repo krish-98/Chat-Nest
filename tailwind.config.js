a387a9cd3fb247b8cb0f2e93e00c97e44f0bddf9
krish-98 <muralikrishnanm98@gmail.com>
2022-10-20 13:31:32 +0530

    configuring the colors for the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        main: "",
+        background: "#FDD2BF",
+        text: "#DF5E5E",
+        layer: "#E98580",
+      },
       flex: {
         2: "2 1 0%",
       },
